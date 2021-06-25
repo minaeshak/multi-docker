473e32098a1c7cc3cb1f11e97806e0409722548a
minaeshak <mina.eshak.11@gmail.com>
2021-06-25 08:07:09 +0000

    deploy on AWS 4

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ function App() {
           >
             Learn React
           </a>
-          <Link to="/">HOOOOOOOME</Link>
+          <Link to="/">HOOOOOME</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
