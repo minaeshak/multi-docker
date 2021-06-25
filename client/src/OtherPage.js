5536cb7263ff7351af733952cbc2b94a105a4b6d
minaeshak <mina.eshak.11@gmail.com>
2021-06-25 08:41:05 +0000

    deploy on AWS 6

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default () => {
   return (
     <div>
-      Im some other page!
+      Thanks Mina I'm the other Page!
       <Link to="/">Go back home</Link>
     </div>
   );
